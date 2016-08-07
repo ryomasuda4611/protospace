7d120f6a87f76413e3aa7b02291f17a9b13305d6
masudaryo4611 <mrra4611@gmial.com>
2016-08-07 15:30:05 +0900

    user情報更新機能の追加

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'bootstrap-sass'
 
 gem 'devise'
 
+gem 'pry-rails'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,15 @@ class UsersController < ApplicationController
 
   def edit
   end
+
+  def update
+    current_user.update(update_params)
+    redirect_to :controller => 'prototypes', :action => 'index'
+  end
+
+  private
+
+  def update_params
+    params.require(:user).permit(:username, :avatar, :password, :profile, :member, :works)
+  end
 end
