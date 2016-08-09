822ce49a7914b7ddf860f35a5e3da74b87bc6dda
masudaryo4611 <mrra4611@gmial.com>
2016-08-09 16:51:03 +0900

    introduce avatar_uploader for user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160808070021) do
+ActiveRecord::Schema.define(version: 20160809074724) do
 
   create_table "prototype_images", force: :cascade do |t|
     t.integer  "prototype_id", limit: 4
@@ -22,12 +22,13 @@ ActiveRecord::Schema.define(version: 20160808070021) do
   end
 
   create_table "prototypes", force: :cascade do |t|
-    t.string   "title",      limit: 255
-    t.string   "catch_copy", limit: 255
-    t.text     "concept",    limit: 65535
-    t.integer  "user_id",    limit: 4
+    t.string   "title",           limit: 255
+    t.string   "catch_copy",      limit: 255
+    t.text     "concept",         limit: 65535
+    t.integer  "user_id",         limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "prototype_image", limit: 255
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,12 +43,12 @@ ActiveRecord::Schema.define(version: 20160808070021) do
     t.string   "current_sign_in_ip",     limit: 255
     t.string   "last_sign_in_ip",        limit: 255
     t.string   "username",               limit: 255
-    t.text     "avatar",                 limit: 65535
     t.text     "profile",                limit: 65535
     t.string   "member",                 limit: 255
     t.text     "works",                  limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar",                 limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
