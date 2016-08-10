38adb98b07a18526a36a1f8f5a00be0681b8386b
masudaryo4611 <mrra4611@gmial.com>
2016-08-10 19:46:24 +0900

    introduce reject_if method at prototype.rb

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-
+  before_action :authenticate_user!, except: [:index]
   def index
     @prototypes = Prototype.includes(:user)
   end
--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,7 +1,8 @@
 class Prototype < ActiveRecord::Base
   has_many :prototype_images
   belongs_to :user
-  accepts_nested_attributes_for :prototype_images
+  accepts_nested_attributes_for :prototype_images, reject_if: proc { |attributes| attributes['image'].blank? }
+end
   def main_image
     if prototype_images.main.present?
     prototype_images.main[0].image
