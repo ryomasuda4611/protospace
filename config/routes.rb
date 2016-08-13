de97712fd63433bfbe5e6693c7d3936cb31deec4
masudaryo4611 <mrra4611@gmial.com>
2016-08-13 16:30:51 +0900

    editted_views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'prototypes#index'
   devise_for :users
-  resources :users, only: [:show, :edit, :index,:update] do
-    resources :prototypes, only:[:show, :new, :create, :edit]
+  resources :users, only: [:show, :edit,:update] do
+    resources :prototypes, only: [:show, :new, :create, :edit]
+  
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160810061628) do
+ActiveRecord::Schema.define(version: 20160813035040) do
 
   create_table "prototype_images", force: :cascade do |t|
     t.integer  "prototype_id", limit: 4
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160810061628) do
     t.datetime "updated_at"
     t.text     "image_url",    limit: 65535
     t.integer  "status",       limit: 4
+    t.string   "image",        limit: 255
   end
 
   add_index "prototype_images", ["prototype_id"], name: "index_prototype_images_on_prototype_id", using: :btree
