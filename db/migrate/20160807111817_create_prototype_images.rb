2bbecd0f901b01f323d93f6d84b3531f7144e8a0
masudaryo4611 <mrra4611@gmial.com>
2016-08-16 20:17:44 +0900

    delete files

--- db/migrate/20160807111817_create_prototype_images.rb
+++
@@ -1,8 +0,0 @@
-class CreatePrototypeImages < ActiveRecord::Migration
-  def change
-    create_table :prototype_images do |t|
-      t.references :prototype, index: true, foreign_key: true
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20160807114650_add_column_to_prototype_image.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToPrototypeImage < ActiveRecord::Migration
-  def change
-    add_column :prototype_images, :image_url , :text
-  end
-end
