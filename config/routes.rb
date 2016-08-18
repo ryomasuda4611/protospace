8007560fc943c60638b50263a29ad52ebaca8066
masudaryo4611 <mrra4611@gmial.com>
2016-08-18 17:27:39 +0900

    userとprototypeのroutingのネストを解除した（特に必要ないと判断したため）

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'prototypes#index'
   devise_for :users
-  resources :users, only: [:show, :edit,:update] do
-    resources :prototypes, except: [:index]
-  end
+  resources :users, only: [:show, :edit,:update]
+  resources :prototypes, except: [:index]
 end
