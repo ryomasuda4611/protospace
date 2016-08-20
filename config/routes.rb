979ecee73e93986d7a2e09dfec3d2da965586b52
masudaryo4611 <mrra4611@gmial.com>
2016-08-20 19:23:12 +0900

    Introduce scope module in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root 'prototypes#index'
   devise_for :users
   resources :users, only: [:show, :edit,:update]
-  resources :prototypes, except: [:index] do
-    resources :likes, only: [:create, :destroy]
+  resources :prototypes do
+    scope module: :prototypes do
+      resources :likes, only: [:create, :destroy]
+    end
   end
 end
