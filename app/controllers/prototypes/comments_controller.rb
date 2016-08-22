693f0215d50482f8f0b1fc5ffbbaf3461d5b82d2
masudaryo4611 <mrra4611@gmial.com>
2016-08-22 14:25:27 +0900

    Define actions in comments_controller

--- app/controllers/prototypes/comments_controller.rb
+++ app/controllers/prototypes/comments_controller.rb
@@ -1,2 +1,25 @@
 class Prototypes::CommentsController < ApplicationController
+before_action :set_prototype, only [:create, :destroy]
+after_action :set_comments, only [:create, :destroy]
+  def create
+    @comment = @prototype.comments.create(create_params)
+  end
+
+  def destroy
+    Comment.find(params[:id]).destory if user_id == current_user.id
+  end
+
+private
+
+  def comment_params
+    params.require(:comment).permit(:review)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find_by(prototype_id: params[:prototype_id])
+  end
+
+  def set_comments
+    @comments = @prototype.comments.includes(:user, :prototype)
+  end
 end
