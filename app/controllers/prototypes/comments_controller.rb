4ba92826ec6d84c045bda6fcd52e1b71f4e57754
masudaryo4611 <mrra4611@gmial.com>
2016-08-22 18:16:31 +0900

    Edit  comments_controller

--- app/controllers/prototypes/comments_controller.rb
+++ app/controllers/prototypes/comments_controller.rb
@@ -1,22 +1,21 @@
 class Prototypes::CommentsController < ApplicationController
-before_action :set_prototype, only [:create, :destroy]
-after_action :set_comments, only [:create, :destroy]
+before_action :set_prototype, only: [:create, :destroy]
+before_action :set_comments, only: [:create, :destroy]
   def create
-    @comment = @prototype.comments.create(create_params)
+    @comment = @prototype.comments.create(comment_params)
   end
 
   def destroy
-    Comment.find(params[:id]).destory if user_id == current_user.id
+    Comment.find(params[:id]).destroy
   end
 
 private
-
   def comment_params
     params.require(:comment).permit(:review, :user_id, :prototype_id)
   end
 
   def set_prototype
-    @prototype = Prototype.find_by(prototype_id: params[:prototype_id])
+    @prototype = Prototype.find(params[:prototype_id])
   end
 
   def set_comments
