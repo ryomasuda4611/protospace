aca74888e054c945a6bc7595007c27062b2c7527
masudaryo4611 <mrra4611@gmial.com>
2016-08-28 18:56:45 +0900

    Create uploader.js to introduce  a function of showing images

---
+++ app/assets/javascripts/uploader.js
@@ -0,0 +1,22 @@
+$(function() {
+  $('.js-image-upload').each(function() {
+    var preview = $(this);
+    var prototype_image = $(this).find('input[type=file]');
+    prototype_image.change(function() {
+      if (!this.files.length) {
+        alert('画像を選んでください')
+      }
+      var file = this.files[0]
+      if (!file.type.match('image.*')) {
+        alert('画像ファイルを選んでください')
+      }
+      const fileReader = new FileReader();
+      fileReader.onload = function() {
+        preview.css({
+          'background-image': 'url(' + fileReader.result + ')'
+        });
+      };
+      fileReader.readAsDataURL(file);
+    });
+  });
+});
