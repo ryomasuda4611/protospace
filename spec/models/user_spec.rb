74384c875b5d147826b69ca31f565e7a6a18d6cb
masudaryo4611 <mrra4611@gmial.com>
2016-09-02 13:21:02 +0900

    Set Tests that expect user is invalid when user's email is dupulicated and user's password is less than 6 characters

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,25 +8,38 @@ describe User do
   end
   it "is invalid without email " do
     user[:email] = ''
-    expect(user).not_to be_valid
+    user.valid?
     expect(user.errors[:email]).to be_present
     end
 
   it "is invalid without username" do
     user[:username] = ''
-    expect(user).not_to be_valid
+    user.valid?
     expect(user.errors[:username]).to be_present
   end
 
   it "is invalid without a password" do
       user = build(:user, password: nil )
-      expect(user).not_to be_valid
+      user.valid?
       expect(user.errors[:password]).to include("can't be blank")
   end
 
   it "is invalid without a password_confirmation" do
     user = build(:user, password_confirmation: "")
-    expect(user).not_to be_valid
+    user.valid?
     expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+  end
+
+  it "is invalid with duplicate email adresses" do
+    user = create(:user)
+    another_user = build(:user, email: user[:email])
+    another_user.valid?
+    expect(another_user.errors[:email]).to include("has already been taken")
+  end
+
+  it "is invalid with a password taht has less than 6 characters" do
+    user = build(:user, password: "a" * 5, password_confirmation: "a" * 5)
+    user.valid?
+    expect(user.errors[:password][0]).to include("is too short")
   end
 end
