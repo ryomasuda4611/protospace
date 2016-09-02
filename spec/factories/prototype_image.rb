aa8798a01ce6049e129e0889cdaa2793185f4ac5
masudaryo4611 <mrra4611@gmial.com>
2016-09-02 19:28:47 +0900

    Create a factory file for prototype_image

---
+++ spec/factories/prototype_image.rb
@@ -0,0 +1,13 @@
+include ActionDispatch::TestProcess
+FactoryGirl.define do
+  factory :prototype_image do
+    image { fixture_file_upload("spec/fixtures/image/test.jpg") }
+    trait :main do
+      status :main
+    end
+
+    trait :sub do
+      status :sub
+    end
+  end
+end
