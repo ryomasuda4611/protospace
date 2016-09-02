a00509c3fefb2b2affd8fbe1e0742616279d91a6
masudaryo4611 <mrra4611@gmial.com>
2016-09-02 19:27:36 +0900

    Defined tests for association between prototype and user, like and prototype_image.

--- spec/models/prototype_spec.rb
+++ spec/models/prototype_spec.rb
@@ -5,11 +5,19 @@ describe Prototype do
 
   describe 'validation' do
     it 'is valid' do
-    expect(prototype).to be_valid
+      expect(prototype).to be_valid
     end
 
     %w{ title concept catch_copy }.each do |column_name|
       it { should validate_presence_of(column_name).with_message("can't be blank")}
     end
+  end
+
+  describe 'association' do
+    it{should belong_to(:user)}
+
+    it{should have_many(:likes).dependent(:destroy)}
+
+    it{should have_many(:prototype_images).dependent(:destroy)}
   end
 end
