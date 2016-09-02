6f6e5ba52e6f4f40088e47435624d065dd02eddb
masudaryo4611 <mrra4611@gmial.com>
2016-09-02 12:47:53 +0900

    Set password and password_confirmation using faker in factories/user.rb

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,9 @@
 FactoryGirl.define do
   factory :user  do
-    password {Faker::Internet.password(8)}
+    password = Faker::Internet.password(8)
     email {Faker::Internet.email}
     username {Faker::Internet.user_name}
+    password password
+    password_confirmation password
   end
 end
