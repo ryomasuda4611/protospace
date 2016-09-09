7182b959f8f4902955bed76b9c8662030ef14e7c
masudaryo4611 <mrra4611@gmial.com>
2016-09-09 14:15:24 +0900

    I loved shoulda but she's gone

--- spec/models/prototype_spec.rb
+++ spec/models/prototype_spec.rb
@@ -8,41 +8,74 @@ describe Prototype do
     end
 
     %w{ title concept catch_copy }.each do |column_name|
-      it { should validate_presence_of(column_name).with_message("can't be blank") }
+      it "is invalid without #{column_name}"  do
+        prototype[column_name] = ""
+        expect(prototype).not_to be_valid
+      end
     end
   end
 
   describe 'association' do
-    it{ should belong_to(:user) }
+    let(:user){ create(:user) }
+    let(:prototype){ create(:prototype)}
+    context 'with user' do
+      it 'associate with user' do
+        prototype = build(:prototype, user: user)
+        expect(prototype.user).to eq user
+      end
+    end
 
-    it{ should have_many(:likes).dependent(:destroy) }
+    context 'with comment' do
+      it 'is deleted with comments' do
+        comment = create(:comment, prototype: prototype)
+        expect{ prototype.destroy }.to change{ Comment.count }
+      end
+    end
 
-    it{ should have_many(:prototype_images).dependent(:destroy) }
+    context 'with prototype_image' do
+      it 'is deleted with prototype_images' do
+        prototype_image = create(:prototype_image, :main, prototype: prototype)
+        expect{ prototype.destroy }.to change{PrototypeImage.count}
+      end
+    end
+    context 'with like' do
+      it 'is deleted with likes' do
+        like = create(:like, prototype: prototype)
+        expect{ prototype.destroy }.to change{ Like.count }
+      end
+    end
   end
-
   describe 'method_test' do
     let(:user){ create(:user) }
     let(:prototype){ create(:prototype, user: user) }
     describe 'like_user' do
-      it 'return true when the user likes the prototype' do
-        like = create(:like, user: user, prototype: prototype)
-        expect(prototype.like_user(user)).to be_truthy
+      context 'when the user likes the prototype' do
+        it 'return true ' do
+          like = create(:like, user: user, prototype: prototype)
+          expect(prototype.like_user(user)).to be_truthy
+        end
       end
 
-      it 'return false when the user does not like the prototype' do
-        another_user = create(:user)
-        like = create(:like, user: another_user, prototype: prototype)
-        expect(prototype.like_user(user)).to be_falsey
+      context 'when the user does not like the prototype' do
+        it 'return false' do
+          another_user = create(:user)
+          like = create(:like, user: another_user, prototype: prototype)
+          expect(prototype.like_user(user)).to be_falsey
+        end
       end
     end
     describe 'main_image' do
-      it 'return true when status is main' do
-        prototype_image = create(:prototype_image, :main, prototype: prototype)
-        expect(prototype.main_image).to be_truthy
+      context 'when status of image is main' do
+        it 'return true' do
+          prototype_image = create(:prototype_image, :main, prototype: prototype)
+          expect(prototype.main_image).to be_truthy
+        end
       end
-      it 'return false when status is sub' do
-        prototype_image = create(:prototype_image, :sub, prototype: prototype)
-        expect(prototype.main_image).to be_falsey
+      context 'when status of image is sub' do
+        it 'return false' do
+          prototype_image = create(:prototype_image, :sub, prototype: prototype)
+          expect(prototype.main_image).to be_falsey
+        end
       end
     end
   end
