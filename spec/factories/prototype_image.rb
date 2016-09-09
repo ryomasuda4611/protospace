429627abe66ea0b15b0e9bd7f77a99da46e61516
masudaryo4611 <mrra4611@gmial.com>
2016-09-09 14:33:26 +0900

    Correction

--- spec/factories/prototype_image.rb
+++ spec/factories/prototype_image.rb
@@ -1,6 +1,6 @@
 include ActionDispatch::TestProcess
 FactoryGirl.define do
-  factory :prototype_image do
+  factory :prototypeImage do
     image { fixture_file_upload("spec/fixtures/image/test.jpg") }
     trait :main do
       status :main
