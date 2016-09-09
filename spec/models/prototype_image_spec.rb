17ea847d301d155f1dcb516940de2671fa515b2c
masudaryo4611 <mrra4611@gmial.com>
2016-09-09 15:48:32 +0900

    I've loved shoulda but she's gone

--- spec/models/prototype_image_spec.rb
+++ spec/models/prototype_image_spec.rb
@@ -1,20 +1,30 @@
 require 'rails_helper'
 
 describe PrototypeImage do
-  let(:prototype_image){ build(:prototype_image, :main) }
+  let(:prototypeImage){ build(:prototypeImage, :main) }
 
   describe 'validation' do
     it 'is valid' do
-      expect(prototype_image).to be_valid
+      expect(prototypeImage).to be_valid
     end
-    it { should validate_presence_of(:image).with_message("can't be blank") }
-    it { should validate_presence_of(:status).with_message("can't be blank") }
+
+    it 'is invalid without status' do
+      prototypeImage[:status] = nil
+      expect(prototypeImage).not_to be_valid
+    end
+
+    it 'is invalid without image' do
+      prototypeImage = build(:prototypeImage, :main, image: nil)
+      prototypeImage[:image] = nil
+      expect(prototypeImage).not_to be_valid
+    end
   end
 
   describe 'association' do
-    it{ should belong_to (:prototype) }
-  end
-  describe 'enum' do
-    it { should define_enum_for(:status).with([:main, :sub]) }
+    it 'associate with prototype' do
+      prototype = build(:prototype)
+      prototypeImage = build(:prototypeImage, prototype: prototype)
+      expect(prototypeImage.prototype).to eq prototype
+    end
   end
 end
