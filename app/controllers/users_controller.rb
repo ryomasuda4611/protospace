288e5eed3b6544f4702a0000cc4877f03d7ba05d
masudaryo4611 <mrra4611@gmial.com>
2016-09-09 14:06:28 +0900

    Defined types of storage depend on environment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :authenticate_user! ,only: [:show, :update, :edit]
+  # before_action :authenticate_user! ,only: [:show, :update, :edit]
 
   def show
     @user = User.find(params[:id])
