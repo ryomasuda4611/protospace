3969fcd0cc1af460e9ca730e47b91ad8736aff04
masudaryo4611 <mrra4611@gmial.com>
2016-09-14 14:04:47 +0900

    Correction (Add page(params[:page])) to index action in Popular controller

--- app/controllers/prototypes/popular_controller.rb
+++ app/controllers/prototypes/popular_controller.rb
@@ -1,7 +1,7 @@
 class Prototypes::PopularController < ApplicationController
 
   def index
-    @prototypes = Prototype.includes(:user).order("prototypes.likes_counter DESC")
-    render template: "prototypes/index"
+    @prototypes = Prototype.includes(:user).order("prototypes.likes_counter DESC").page(params[:page]).per(5)
+    render 'prototypes/index'
   end
 end
